f98b08eb75282a97b0750e54b1ea8945f9712056
EdgarACarneiro <up201503784@fe.up.pt>
2019-02-28 18:23:47 +0000

    Mssi class thinking

--- Projects/Project_1/BioSeq.py
+++ Projects/Project_1/BioSeq.py
@@ -5,7 +5,7 @@ from abc import ABC, abstractmethod
 class BioSeq:
     """Class that represents a Bio Sequence: can either be a Dna, Rna or Protein"""
 
-    class _SeqType(Enum):
+    class _BioType(Enum):
         """Possible Bio Sequence types"""
         DNA = "DNA"
         RNA = "RNA"
@@ -16,23 +16,26 @@ class BioSeq:
         pass
 
     def __init__(self, seq, seq_type="DNA"):
-        seq_type = self._SeqType(seq_type)
-
-        if seq_type is self._SeqType.DNA:
-            __bio_seq = _Dna(seq)
-
-        elif seq_type is self._SeqType.RNA:
-            __bio_seq = _Rna(seq)
-
-        elif seq_type is self._SeqType.PROTEIN:
-            __bio_seq = _Protein(seq)
-
-        else:
+        # Assuring it is either Dna, Rna or a Protein
+        if seq_type.upper() not in [el.value for el in list(self._BioType)]:
             raise self.BioTypeException
 
+        bio_type = self._BioType(seq_type)
 
+        if bio_type is self._BioType.DNA:
+            __bio_seq = _Dna(seq)
+        elif seq_type is self._BioType.RNA:
+            __bio_seq = _Rna(seq)
+        else:
+            __bio_seq = _Protein(seq)
+
+# Não preciso disto -> podem herdar de bioseq fds
 class _Seq(ABC):
     """Abstract class that represents a sequence"""
+
+    class InvalidSequenceException(Exception):
+        """Error raised when sequence contains invalid characters"""
+        pass
 
     def __init__(self, seq):
         self.__seq = seq
@@ -45,14 +48,14 @@ class _Dna(_Seq):
                 "C": "G"}
 
     def __init__(self, seq):
-        self.Seq.__init__(self, seq)
+        super().__init__(seq)
 
 
 class _Rna(_Seq):
     def __init__(self, seq):
-        self.Seq.__init__(self, seq)
+        super().__init__(seq)
 
 
 class _Protein(_Seq):
     def __init__(self, seq):
-        self.Seq.__init__(self, seq)
+        super().__init__(seq)
