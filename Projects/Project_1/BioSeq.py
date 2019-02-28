bfefc59f982d1e9dc2134829f70a0ae454f0cc50
EdgarACarneiro <up201503784@fe.up.pt>
2019-02-28 12:22:00 +0000

    Base classes and structure

--- Projects/Project_1/BioSeq.py
+++ Projects/Project_1/BioSeq.py
@@ -1,12 +1,58 @@
 from enum import Enum
+from abc import ABC, abstractmethod
+
 
 class BioSeq:
+    """Class that represents a Bio Sequence: can either be a Dna, Rna or Protein"""
 
     class _SeqType(Enum):
+        """Possible Bio Sequence types"""
         DNA = "DNA"
         RNA = "RNA"
         PROTEIN = "PROTEIN"
 
+    class BioTypeException(Exception):
+        """Error raised when bio sequence type is not recognized"""
+        pass
+
     def __init__(self, seq, seq_type="DNA"):
-        self.__seq__ = seq
-        self.__seq__type__ = self.SeqType(seq_type)
+        seq_type = self._SeqType(seq_type)
+
+        if seq_type is self._SeqType.DNA:
+            __bio_seq = _Dna(seq)
+
+        elif seq_type is self._SeqType.RNA:
+            __bio_seq = _Rna(seq)
+
+        elif seq_type is self._SeqType.PROTEIN:
+            __bio_seq = _Protein(seq)
+
+        else:
+            raise self.BioTypeException
+
+
+class _Seq(ABC):
+    """Abstract class that represents a sequence"""
+
+    def __init__(self, seq):
+        self.__seq = seq
+
+
+class _Dna(_Seq):
+    switcher = {"A": "T",
+                "T": "A",
+                "G": "C",
+                "C": "G"}
+
+    def __init__(self, seq):
+        self.Seq.__init__(self, seq)
+
+
+class _Rna(_Seq):
+    def __init__(self, seq):
+        self.Seq.__init__(self, seq)
+
+
+class _Protein(_Seq):
+    def __init__(self, seq):
+        self.Seq.__init__(self, seq)
