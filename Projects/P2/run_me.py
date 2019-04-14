61a6b74eb824767539d8ec3af80f73bb7ec4ad77
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-04-14 21:47:13 +0100

    run_me.py at 50%

--- Projects/P2/run_me.py
+++ Projects/P2/run_me.py
@@ -1,90 +1,108 @@
-from seq_align import SeqAlign
+from bioseq import BioSeq
+from seq_align import read_submat_file,\
+                        subst_matrix,\
+                        pretty_matrix,\
+                        global_align_multiple_solutions,\
+                        recover_global_align_multiple_solutions,\
+                        local_align_multiple_solutions,\
+                        recover_local_align_multiple_solutions,\
+                        compare_pairwise_global_align,\
+                        compare_pairwise_local_align
 
 def wait_input():
-    input("Press Enter to continue...")
+    input("Press Enter to continue...\n")
 
 if __name__ == '__main__':
-    # pretty_matrix(ga_score, " " + self.seq1, " " + self.seq2)
-    # pretty_matrix(ga_trace, " " + self.seq1, " " + self.seq2)
-    print("Hello and welcome to the demonstrarion of the module 'BioSeq'!\n")
+
+    print("Hello and welcome to the demonstrarion of the module 'SeqAlign'!\n")
     print("The program will stop at checkpoints and you, as the User, will have to press Enter to continue. Ok?\n")
-    input("Press Enter to continue...")
+    wait_input()
 
-    print("Ok, nice!\n Lets create a dna sequence with the sequence 'AGCTAGCTAGCTACATCAAACGATCGTCGCTAGCTAGCTAC', ok?\n")
-    input("Press Enter to continue...")
+    print("Ok, nice!\nLets start with two sequences: 'GATTACA' & 'GCATGCT', ok?\n")
+    wait_input()
 
-    dna = BioSeq.create_bio_seq("AGCTAGCTAGCTACATCAAACGATCGTCGCTAGCTAGCTAC")
-    dna.pretty_print()
+    seq1 = 'GATTACA'
+    seq2 = 'GCATGCT'
 
-    print("Now, lets load a genetic_code dictionary!\n")
-    dna.read_genetic_code('test/files/genetic_code.txt')
-    input("Press Enter to continue...")
-    dna.pretty_print()
+    print("Now we can either create a substitution matrix or read one from a file!\n")
+    print("Lets create one first with the alphabet 'ACTG' and 1 for matches and -1 for mismatches.\n")
+    wait_input()
 
-    input("Press Enter to continue...")
-    print("Ok, look, we can also transcript our dna into a RNA!\n")
-    print(str(dna.transcription()))
+    sm_dna = subst_matrix("ACGT", 1, -1)
+    print(sm_dna)
+    print("We will call this substitution matrix 'sm_dna', ok?\n")
+    wait_input()
 
-    input("Press Enter to continue...")
-    print("We do have a genetic code dictionary and a dna, so we will translate it now!\n")
-    input("Press Enter to continue...")
-    print(str(dna.translate()))
+    print("Now, lets load one from a file.\nCan we use the one in the file 'tests/files/blosum62.mat'? Is it okay for you?\n")
+    wait_input()
 
-    input("Press Enter to continue...")
-    print("We can also see the codon usage of our sequence for the 'R' aminoacid!\n")
-    input("Press Enter to continue...")
-    print(dna.codon_usage("R"))
+    sm_blosum = read_submat_file('tests/files/blosum62.mat')
+    print(sm_blosum)
+    print("We will call this substitution matrix 'sm_blosum', ok?\n")
+    wait_input()
 
-    input("Press Enter to continue...")
-    print("Lets see all the obtainable reading frames!\n")
-    input("Press Enter to continue...")
-    for rf in dna.reading_frames():
-        print(str(rf))
+    print("Since we have a substitution matrix and the sequences we can get some global and local alignments!\n\n")
+    print("::: GLOBAL ALIGNMENT with multiple solutions :::\n")
+    print("\nLets make the global alignment with our sequences, the 'sm_dna' and a gap of -1, shall we?\n")
+    wait_input()
 
-    input("Press Enter to continue...")
-    print("We can now try other dna sequence! What about this one? 'ATGAAATTATGAATGAGCCTCAGCTGAAGCATCGCGCATCAGACTACGCTCAGACTCAGACTCAGCATTATAGTGAATGTTAATAAATAAAATAA' \n")
-    input("Press Enter to continue...")
-    print("Lets see all its opeaning reading frames!\n")
-    dna = BioSeq.create_bio_seq("ATGAAATTATGAATGAGCCTCAGCTGAAGCATCGCGCATCAGACTACGCTCAGACTCAGACTCAGCATTATAGTGAATGTTAATAAATAAAATAA")
-    dna.read_genetic_code('test/files/genetic_code.txt')
+    ga_score, ga_trace = global_align_multiple_solutions(seq1, seq2, sm_dna, -1)
+    print("Score matrix obtained:\n")
+    pretty_matrix(ga_score, " " + seq1, " " + seq2)
+    wait_input()
 
-    for orfs in dna.all_orfs():
-        print(orfs.get_seq())
+    print("\nTrace matrix obtained:\n")
+    pretty_matrix(ga_trace, " " + seq1, " " + seq2)
+    wait_input()
 
-    input("Press Enter to continue...")
+    print("Using the computed matrixes we can recover the multipal optimal global alignements.\nLets do it?\n")
+    wait_input()
+
+    rga = recover_global_align_multiple_solutions(ga_trace, seq1, seq2)
+    print("The obtained global alignments were:\n")
+    for align in rga:
+        print('Seq1: ' + str(align[0]) +  '\nSeq2: ' + str(align[1]) + '\n')
+    wait_input()
+
+    print("::: LOCAL ALIGNMENT with multiple solutions :::\n")
+    print("\nLets make the local alignment with our sequences, the 'sm_dna' and a gap of -1, shall we?\n")
+    wait_input()
+
+    ga_score, ga_trace, max_score = local_align_multiple_solutions(seq1, seq2, sm_dna, -1)
+    print("Score matrix obtained:\n")
+    pretty_matrix(ga_score, " " + seq1, " " + seq2)
+    wait_input()
+
+    print("\nTrace matrix obtained:\n")
+    pretty_matrix(ga_trace, " " + seq1, " " + seq2)
+    wait_input()
+
+    print("\nMaximum value in the score matrix: " + str(max_score) + '\n')
+    wait_input()
+
+    print("Using the computed matrixes we can recover the multipal optimal local alignements.\nLets do it?\n")
+    wait_input()
+
+    rga = recover_local_align_multiple_solutions(ga_score, ga_trace, seq1, seq2)
+    print("The obtained local alignments were:\n")
+    for align in rga:
+        print('Seq1: ' + str(align[0]) +  '\nSeq2: ' + str(align[1]) + '\n')
+    wait_input()
 
     print("\n-------------------------\n")
 
-    print("Hey, lets try an RNA now! Can it be this one? 'UCGA'?\n")
-    rna = BioSeq.create_bio_seq("UCGA", "rna")
-    
-    input("Press Enter to continue...")
-    print("Lets see the frequency of its symbols!\n")
-    for (k, v) in rna.freq_symbols().items():
-        print(k + ': ' + str(v))
-    input("Press Enter to continue...")
+    print("Now shall we make it a little bit more complex?\n")
+    wait_input()
 
-    print("Lets see its gc percent!\n")
-    print(rna.gc_percent())
-    input("Press Enter to continue...")
+    print("So, lets load and use the proteins present in the 'tests/files/protein_sequences.fas' file!\n\
+        We need to use the read fasta functionality from the bioseq library to load them.\n")
+    wait_input()
 
-    print("And what about its reverse complement?\n")
-    print("Reverse Complement: " + rna.reverse_complement())
-    input("Press Enter to continue...")
+    print("The protein sequences are:\n")
+    seqs = BioSeq.read_fasta_file('tests/files/protein_sequences.fas')
+    for key, value in seqs.items():
+        print("> " + str(key) + ": " + str(value) + "\n")
+    wait_input()
 
-    print("We can also save and load sequences! Let's do it with the RNA to the 'test/files/test_save_load.csv' file!\n")
-    input("Press Enter to continue...")
-    rna.save('test/files/test_save_load.csv')
-    print("The rna is now saved, open the csv!\n")
-    input("And now we print what we load from that file!\n")
-    BioSeq.load('test/files/test_save_load.csv').pretty_print()
 
-    input("Press Enter to continue...")
-
-    print("\n-------------------------\n")
-
-    print("Finally lets see the contents of the FASTA file in the 'tests/files/sequence.fasta'!\n")
-    input("Press Enter to continue...")
-    fasta = BioSeq.read_fasta_file('test/files/sequence.fasta')
-    for (k, v) in fasta.items():
-        print('> ' + k + '\n' + v + '\n\n')
+    print("")
