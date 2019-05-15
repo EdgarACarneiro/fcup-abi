a4859e019cf6bd4377d22d96c6883afb4525377b
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-05-15 11:29:20 +0100

    Class 15/5

--- CodeGiven2/HierarchicalClustering.py
+++ CodeGiven2/HierarchicalClustering.py
@@ -1,48 +1,52 @@
-from BinaryTree import BinaryTree
+from UltrametricTree import UltrametricTree
 from NumMatrix import NumMatrix
+
 
 class HierarchicalClustering:
 
+    matdists: NumMatrix
+
     def __init__(self, matdists):
         self.matdists = matdists
-    
+
     def execute_clustering(self):
         # initialize the trees
         trees = []
         for i in range(self.matdists.num_rows()):
-            # create a tree for each leaf
-            # add to list of trees
-            # ... 
+            trees.append(UltrametricTree(i))  # All leafs
+
         # make a copy of the distance matrix to change it
-        tableDist = # ...
+        tableDist = self.matdists.copy()
+
         # iterations
         for k in range(self.matdists.num_rows(), 1, -1):
-            # indices in the matrix for the minimum distance
-            mins = # ...
-            i,j = mins[0], mins[1]
+            mins = tableDist.min_dist_indexes()
+            i, j = mins[0], mins[1]
+
             # create a new tree joining the clusters
-            n = BinaryTree(#....)
-            if k>2:
-                # remove trees being joined from the list 
+            n = UltrametricTree(-1, tableDist.get_value(i, j) / 2.0, i, trees[i], trees[j])
+            if k > 2:
+                # remove trees being joined from the list
                 ti = trees.pop(i)
                 tj = trees.pop(j)
                 dists = []
                 # calculate the distance for the new cluster
-                for x in range(tableDist.num_rows()):          
+                for x in range(tableDist.num_rows()):
                     if x != i and x != j:
                         si = len(ti.get_cluster())
                         sj = len(tj.get_cluster())
                         # use the weighted average to calculate the distances between the clusters
-                        d = # ...
+                        d =  # ...
                         dists.append(d)
-                # update the matrix: 
+                # update the matrix:
                 # ...
                 trees.append(n)
-            else: return n
+            else:
+                return n
 
 
 def test():
-    m = NumMatrix(5,5)
+    m = NumMatrix(5, 5)
     m.set_value(0, 1, 2)
     m.set_value(0, 2, 5)
     m.set_value(0, 3, 7)
@@ -56,6 +60,7 @@ def test():
     hc = HierarchicalClustering(m)
     arv = hc.execute_clustering()
     arv.print_tree()
-    
-if __name__ == '__main__': 
+
+
+if __name__ == '__main__':
     test()
---
+++ CodeGiven2/UltrametricTree.py
@@ -0,0 +1,121 @@
+class UltrametricTree:
+
+    def __init__(self, val, dist=0, left=None, right=None):
+        self.value = val
+        self.distance = dist # distance from this node to the leafs
+        self.left = left
+        self.right = right
+
+    def has_left_sibling(self):
+        return self.left != None
+
+    def has_right_sibling(self):
+        return self.right != NotImplemented
+
+    def get_cluster(self):
+        """Get the cluster values => get the tree leaves"""
+        res = []
+
+        if self.value >= 0:
+            res.append(self.value)
+        else:
+            if self.has_left_sibling():
+                res.extend(self.left.get_cluster())
+            if self.has_right_sibling():
+                res.extend(self.right.get_cluster())
+
+        return res
+
+    def print_tree(self):
+        self.print_tree_rec(0, "Root")
+
+    def print_tree_rec(self, level, side):
+        tabs = ""
+        for i in range(level):
+            tabs += "\t"
+        if self.value >= 0:
+            print(tabs, side, " - value:", self.value)
+        else:
+            print(tabs, side, "- Dist.: ", self.distance)
+            if (self.has_left_sibling()):
+                self.left.print_tree_rec(level+1, "Left")
+            if (self.has_right_sibling()):
+                self.right.print_tree_rec(level+1, "Right")
+
+    def size(self):
+        ''' size of the tree: returns two values
+        - number of internal nodes of the tree
+        - number of leaves'''
+        numleaves = 0
+        numnodes = 0
+        if self.value >= 0:
+            numleaves = 1
+        else:
+            if (self.left != None):
+                resl = self.left.size()
+            else:
+                resl = (0, 0)
+            if (self.right != None):
+                resr = self.right.size()
+            else:
+                resr = (0, 0)
+            numnodes += (resl[0] + resr[0] + 1)
+            numleaves += (resl[1] + resr[1])
+        return numnodes, numleaves
+
+    def exists_leaf(self, leafnum) -> bool:
+        """Finds leaf in tree"""
+
+        if self.value >= 0:
+            return leafnum == self.value
+        else:
+            return True if\
+                (self.left.exists_leaf(leafnum) if self.has_left_sibling() else False)\
+                else\
+                (self.right.exists_leaf(leafnum) if self.has_left_sibling() else False)
+
+    def common_ancestor(self, leaf1, leaf2):
+        ''' Return simplest tree that contains leaf1, leaf2'''
+        # Left sibling
+        if self.has_left_sibling():
+            if self.left.exists_leaf(leaf1):
+                if self.left.exists_leaf(leaf2):
+                    return self.left.common_ancestor(leaf1, leaf2)
+                else:
+                    return self
+        # Right sibling
+        if self.has_right_sibling():
+            if self.right.exists_leaf(leaf1):
+                if self.right.exists_leaf(leaf2):
+                    return self.right.common_ancestor(leaf1, leaf2)
+                else:
+                    return self
+
+
+    def distance_leaves(self, leafnum1, leafnum2):
+        ''' distance between leafnum1 and leafnum2 using the common ancestor function.'''
+        return self.common_ancestor(leafnum1, leafnum2).distance * 2
+
+
+def test():
+    a = UltrametricTree(1)
+    b = UltrametricTree(2)
+    c = UltrametricTree(3)
+    d = UltrametricTree(4)
+    e = UltrametricTree(-1, 2.0, b, c)
+    f = UltrametricTree(-1, 1.5, d, a)
+    g = UltrametricTree(-1, 4.5, e, f)
+    g.print_tree()
+    print(g.get_cluster())
+
+    # testing exercise 3
+    print(g.size())
+    print(g.exists_leaf(1))
+    print(g.exists_leaf(5))
+    g.common_ancestor(1, 4).print_tree()
+    print(g.distance_leaves(1, 4))
+    print(g.distance_leaves(1, 2))
+
+
+if __name__ == '__main__':
+    test()
