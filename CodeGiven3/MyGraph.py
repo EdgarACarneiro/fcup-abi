c5ce0b3780bbddb2f8adcb5cf8fcc6529587667c
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2019-06-06 17:50:19 +0100

    Completed mygraph functions

--- CodeGiven3/MyGraph.py
+++ CodeGiven3/MyGraph.py
@@ -39,7 +39,7 @@ class MyGraph:
         ''' Add edge to the graph; if vertices do not exist, they are added to the graph '''
         self.add_node(o)
         self.add_node(d)
-        if d not in self.graph[0]:
+        if d not in self.graph[o]:
             self.graph[o].append(d)
 
     # successors, predecessors, adjacent nodes
@@ -78,22 +78,20 @@ class MyGraph:
     def all_degrees(self, deg_type="inout"):
         ''' Computes the degree (of a given type) for all nodes.
         deg_type can be "in", "out", or "inout" '''
-        degs = {}
-        for v in self.graph.keys():
-            if deg_type == "out" or deg_type == "inout":
-                degs[v] = len(self.graph[v])
-            else:
-                degs[v] = 0
-        if deg_type == "in" or deg_type == "inout":
-            for v in self.graph.keys():
-                for d in self.graph[v]:
-                    if deg_type == "in" or v not in self.graph[d]:
-                        degs[d] = degs[d] + 1
-        return degs
+        assert deg_type in ["inout", "in", "out"], "Invalid degree type"
+
+        switcher = {
+            "in": lambda v: self.in_degree(v),
+            "out": lambda v: self.out_degree(v),
+            "inout": lambda v: self.in_degree(v) + self.out_degree(v)
+        }
+
+        return {v: switcher[deg_type](v) for v in self.graph.keys()}
 
     def highest_degrees(self, all_deg=None, deg_type="inout", top=10):
         if all_deg is None:
             all_deg = self.all_degrees(deg_type)
+
         ord_deg = sorted(list(all_deg.items()),
                          key=lambda x: x[1], reverse=True)
         return list(map(lambda x: x[0], ord_deg[:top]))
@@ -101,14 +99,22 @@ class MyGraph:
     # topological metrics over degrees
 
     def mean_degree(self, deg_type="inout"):
-        ''' average degree of all nodes: sum of all degrees divided by number of nodes'''
-        pass
+        return sum(self.all_degrees(deg_type).values()) / self.size()[0]
 
     def prob_degree(self, deg_type="inout"):
         # count the number of occurrences of each degree in the network and derive its frequencies
-        pass
+        probs = {}
+        n_nodes = self.size()[0]
 
-        # BFS and DFS searches
+        for deg in self.all_degrees(deg_type).values():
+            if (deg in probs):
+                probs[deg] += 1 / n_nodes
+            else:
+                probs[deg] = 1 / n_nodes
+
+        return probs
+
+    # BFS and DFS searches
 
     def reachable_bfs(self, v):
         l = [v]   # list of nodes to be handled
@@ -169,28 +175,23 @@ class MyGraph:
     # clustering
 
     def clustering_coef(self, v):
-        adjs = []  # ... get the list of adjancent nodes
+        adjs = self.get_adjacents(v)
         if len(adjs) <= 1:
             return 0.0
+
         # calculate the number of links of the adjacent nodes
-        ligs = 0
         # compare pairwisely if nodes in this list are connected between them
-        for i in adjs:
-            for j in adjs:
-                if i != j:
-                    pass
-                    # check if i and j are connected to each other; if yes increment counter of links
+        ligs = sum([1 for j in adjs
+                    for i in adjs
+                    if i != j and i in self.get_adjacents(j)])
+
         return float(ligs)/(len(adjs)*(len(adjs)-1))
 
     def all_clustering_coefs(self):
-        pass
-        # go through all the nodes and calculate its cc
-        # put those in a dictionary and return
+        return {v: self.clustering_coef(v) for v in self.get_nodes()}
 
     def mean_clustering_coef(self):
-        pass
-        # get all the clustering coefficients
-        # and return the mean of all ccs
+        return self.all_clustering_coefs() / self.size()[0]
 
 
 if __name__ == "__main__":
@@ -206,34 +207,36 @@ if __name__ == "__main__":
     gr.add_edge(4, 2)
     gr.print_graph()
     print(gr.size())
+    print()
+    print(gr.get_successors(2))
+    print(gr.get_predecessors(2))
+    print(gr.get_adjacents(2))
+    print()
+    print(gr.in_degree(2))
+    print(gr.out_degree(2))
+    print(gr.degree(2))
+    print()
+    print(gr.all_degrees("inout"))
+    print(gr.all_degrees("in"))
+    print(gr.all_degrees("out"))
+    print()
+    #
+    gr2 = MyGraph({1: [2, 3, 4], 2: [5, 6], 3: [6, 8],
+                   4: [8], 5: [7], 6: [], 7: [], 8: []})
+    print(gr2.reachable_bfs(1))
+    print(gr2.reachable_dfs(1))
+    print(gr2.distance(1, 7))
+    print(gr2.shortest_path(1, 7))
+    print(gr2.distance(1, 8))
+    print(gr2.shortest_path(1, 8))
+    print(gr2.distance(6, 1))
+    print(gr2.shortest_path(6, 1))
 
-    # print (gr.get_successors(2))
-    # print (gr.get_predecessors(2))
-    # print (gr.get_adjacents(2))
-    #
-    # print (gr.in_degree(2))
-    # print (gr.out_degree(2))
-    # print (gr.degree(2))
-    #
-    # print(gr.all_degrees("inout"))
-    # print(gr.all_degrees("in"))
-    # print(gr.all_degrees("out"))
-    #
-    # gr2 = MyGraph({1:[2,3,4], 2:[5,6],3:[6,8],4:[8],5:[7],6:[],7:[],8:[]})
-    # print(gr2.reachable_bfs(1))
-    # print(gr2.reachable_dfs(1))
-    #
-    # print(gr2.distance(1,7))
-    # print(gr2.shortest_path(1,7))
-    # print(gr2.distance(1,8))
-    # print(gr2.shortest_path(1,8))
-    # print(gr2.distance(6,1))
-    # print(gr2.shortest_path(6,1))
-    #
     # print(gr2.reachable_with_dist(1))
-
-    # print(gr.mean_degree())
-    # print(gr.prob_degree())
+    print()
+    print(gr.mean_degree())
+    print(gr.prob_degree())
     # print(gr.mean_distances())
-    #print (gr.clustering_coef(1))
-    #print (gr.clustering_coef(2))
+    print()
+    print(gr.clustering_coef(1))
+    print(gr.clustering_coef(2))
